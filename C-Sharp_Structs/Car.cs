7dddd3f574f0e291fb65d4c8c7e2d083a8ca8fc5
Malaka98 <malaka.aruna14413@gmail.com>
2021-07-08 18:22:41 +0530

    Add project files.

---
+++ C-Sharp_Structs/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Structs
+{
+    struct Car
+    {
+        internal string color;
+        internal int tire;
+        internal int seat;
+        internal int engine_capacity;
+
+        internal Car(string color, int tire, int seat, int engine_capacity)
+        {
+            this.color = color;
+            this.tire = tire;
+            this.seat = seat;
+            this.engine_capacity = engine_capacity;
+        }
+
+        internal void Details()
+        {
+            Console.WriteLine("Color: " + color);
+            Console.WriteLine("Tire: " + tire);
+            Console.WriteLine("Seat: " + seat);
+            Console.WriteLine("Engine Capacity: " + engine_capacity);
+        }
+        internal void Drive()
+        {
+            Console.WriteLine("The car is moving");
+        }
+
+        internal void Break()
+        {
+            Console.WriteLine("The car stop");
+        }
+
+        internal static void Print()
+        {
+            Console.WriteLine("This is blueprint of car");
+        }
+
+    }
+}
---
+++ C-Sharp_Structs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace C_Sharp_Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car.Print();
+
+            Car obj = new Car("Black", 4, 4, 6000);
+
+            obj.Details();
+            obj.Drive();
+            obj.Break();
+        }
+    }
+}
